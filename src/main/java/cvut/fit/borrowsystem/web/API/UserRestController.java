ad8654e85bda661550c80e850f33f9d8015094cf
Jakub <jakub.tucek@gmail.com>
2016-06-11 18:24:56 +0200

    Add user rest api controller annotation

--- src/main/java/cvut/fit/borrowsystem/web/API/UserRestController.java
+++ src/main/java/cvut/fit/borrowsystem/web/API/UserRestController.java
@@ -4,10 +4,15 @@ import cvut.fit.borrowsystem.service.UserManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by Jakub Tuček on 11.6.2016.
  */
+
+@RestController
+@RequestMapping("/api/users")
 public class UserRestController {
     private static final Logger log = LoggerFactory.getLogger(UserRestController.class);
 
